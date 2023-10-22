6e75f0f713687dbac3baae662e0b07198eba7602
LaggyNacho <57537992+LaggyNacho@users.noreply.github.com>
2023-10-22 17:29:47 +0200

    set up backend

---
+++ backend_service/src/test/java/fm/statify/backend_service/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fm.statify.backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
