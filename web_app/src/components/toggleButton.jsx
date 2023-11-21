00be3a2457552c40ba4fa4256274586abd579c59
Ines Storck <istorck@allegion.com>
2023-11-21 12:34:44 +0100

    Add ToggleButton Component

---
+++ web_app/src/components/toggleButton.jsx
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+import "../css/toggle-button.css";
+import {decodeColors} from "../util/decodeColors";
+import PropTypes from "prop-types";
+import Button from "./button";
+
+export default function ToggleButton({className, color, selected, setSelected, buttonWidth, textSize, choices}){
+
+    let style = {
+        fontSize:textSize + "px",
+        width:buttonWidth + "px"
+    }
+
+    let selectedStyle = {
+        backgroundColor: decodeColors[color],
+        opacity: "100%"
+
+    }
+
+    let unselectedStyle = {
+        backgroundColor: decodeColors[color],
+        opacity: "40%"
+    }
+
+    return(
+        <div className="toggle-button" style={style}>
+            <button className={className}
+                    style={selected === 0 ? selectedStyle : unselectedStyle}
+                    onClick={() => setSelected(0)}
+            >{choices[0]}</button>
+            <button className={className}
+                    style={selected === 1 ? selectedStyle : unselectedStyle}
+                    onClick={() => setSelected(1)}
+            >{choices[1]}</button>
+        </div>
+    )
+}
+
+const arrayOfLength = (expectedLength, props, propName, componentName) => {
+    const arrayPropLength = props[propName].length
+
+    if (arrayPropLength !== expectedLength) {
+        return new Error(
+            `Invalid array length ${arrayPropLength} (expected ${expectedLength}) for prop ${propName} supplied to ${componentName}. Validation failed.`
+        )
+    }
+}
+
+Button.propTypes={
+    color: PropTypes.oneOf(["orange", "green", "purple"]),
+    className: PropTypes.string,
+    selected: PropTypes.oneOf([0, 1]).isRequired,
+    choices: arrayOfLength.bind(null, 2).isRequired
+}
+
+Button.defaultProps={
+    className:"",
+}
+
