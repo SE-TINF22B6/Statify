4c55aa7d3abdd826794fe9ff336d70c9bbd81e33
Ines Storck <istorck@allegion.com>
2024-04-21 21:31:27 +0200

    Add CORS configuration

---
+++ backend_service/src/main/java/fm/statify/backend_service/WebConfig.java
@@ -0,0 +1,17 @@
+package fm.statify.backend_service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
