7db884730e3a09f2218518dd41a4848c485dd58d
Ines Storck <storck.ines@gmail.com>
2024-05-16 13:02:14 +0200

    Updated Statistics Page

--- web_app/src/pages/ProfilePage.jsx
+++ web_app/src/pages/ProfilePage.jsx
@@ -19,7 +19,6 @@ export default function ProfilePage(){
             })
             .then((user) => {
                 setUser(user);
-                console.log(user)
             })
     }, []);
 
--- web_app/src/pages/StatisticsPage.jsx
+++ web_app/src/pages/StatisticsPage.jsx
@@ -17,65 +17,68 @@ export default function StatisticsPage() {
             </Actionbar>
             <div className={"statistics-page page"}>
                 <div className={"content column"}>
-                    <StatisticsFrame header={"Top Tracks"} subheader={"01.04.2024 - 22.04.2024"}>
-                        <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b273b7a9a6a2bf311630d3fc6956"}
-                                       title={"Faith"}
-                                       subtitle={"George Michael"}
-                                       number={1}
-                                       color={"green"}/>
-                        <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b27377a50cb765eef114e8ce488c"}
-                                       title={"Lemon Tree"}
-                                       subtitle={"Fools Garden"}
-                                       number={2}
-                                       color={"orange"}/>
-                        <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b2734121faee8df82c526cbab2be"}
-                                       title={"Thriller"}
-                                       subtitle={"Michael Jackson"}
-                                       number={3}
-                                       color={"orange"}/>
-                        <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b2733b7492bb678d5d51683444ae"}
-                                       title={"Footloose"}
-                                       subtitle={"Kenny Loggins"}
-                                       number={4}
-                                       color={"purple"}/>
-                        <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b2731fc9fd5d701ee05cb39b7b19"}
-                                       title={"Like a Prayer"}
-                                       subtitle={"Madonna"}
-                                       number={5}
-                                       color={"purple"}/>
-                    </StatisticsFrame>
-                    <StatisticsFrame header={"Top Artists"} subheader={"01.04.2024 - 22.04.2024"}>
-                        <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5eb214f3cf1cbe7139c1e26ffbb"}
-                                       title={"The Weeknd"}
-                                       number={1}
-                                       color={"green"}/>
-                        <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5ebe03a98785f3658f0b6461ec4"}
-                                       title={"Olivia Rodrigo"}
-                                       number={2}
-                                       color={"orange"}/>
-                        <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5ebad85a585103dfc2f3439119a"}
-                                       title={"Hozier"}
-                                       number={3}
-                                       color={"orange"}/>
-                        <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5eb40b5c07ab77b6b1a9075fdc0"}
-                                       title={"Ariana Grande"}
-                                       number={4}
-                                       color={"purple"}/>
-                        <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5ebe672b5f553298dcdccb0e676"}
-                                       title={"Taylor Swift"}
-                                       number={5}
-                                       color={"purple"}/>
-                    </StatisticsFrame>
-                    <StatisticsFrame
-                        header={"Playlist Statistics"}>
-                        <PlaylistStatisticsItem name={"My Playlist"}
-                                                minutes={762}
-                                                songs={254}
-                                                top_artist={"Coldplay"}
-                                                top_genre={"Pop"}
-                                                top_artist_count={21}
-                                                top_genre_count={86}/>
-                    </StatisticsFrame>
+                    <div className={"row"}>
+                        <StatisticsFrame header={"Top Tracks"} subheader={"01.04.2024 - 22.04.2024"}>
+                            <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b273b7a9a6a2bf311630d3fc6956"}
+                                           title={"Faith"}
+                                           subtitle={"George Michael"}
+                                           number={1}
+                                           color={"green"}/>
+                            <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b27377a50cb765eef114e8ce488c"}
+                                           title={"Lemon Tree"}
+                                           subtitle={"Fools Garden"}
+                                           number={2}
+                                           color={"orange"}/>
+                            <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b2734121faee8df82c526cbab2be"}
+                                           title={"Thriller"}
+                                           subtitle={"Michael Jackson"}
+                                           number={3}
+                                           color={"orange"}/>
+                            <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b2733b7492bb678d5d51683444ae"}
+                                           title={"Footloose"}
+                                           subtitle={"Kenny Loggins"}
+                                           number={4}
+                                           color={"purple"}/>
+                            <StatisticItem image={"https://i.scdn.co/image/ab67616d0000b2731fc9fd5d701ee05cb39b7b19"}
+                                           title={"Like a Prayer"}
+                                           subtitle={"Madonna"}
+                                           number={5}
+                                           color={"purple"}/>
+                        </StatisticsFrame>
+                        <StatisticsFrame header={"Top Artists"} subheader={"01.04.2024 - 22.04.2024"}>
+                            <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5eb214f3cf1cbe7139c1e26ffbb"}
+                                           title={"The Weeknd"}
+                                           number={1}
+                                           color={"green"}/>
+                            <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5ebe03a98785f3658f0b6461ec4"}
+                                           title={"Olivia Rodrigo"}
+                                           number={2}
+                                           color={"orange"}/>
+                            <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5ebad85a585103dfc2f3439119a"}
+                                           title={"Hozier"}
+                                           number={3}
+                                           color={"orange"}/>
+                            <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5eb40b5c07ab77b6b1a9075fdc0"}
+                                           title={"Ariana Grande"}
+                                           number={4}
+                                           color={"purple"}/>
+                            <StatisticItem image={"https://i.scdn.co/image/ab6761610000e5ebe672b5f553298dcdccb0e676"}
+                                           title={"Taylor Swift"}
+                                           number={5}
+                                           color={"purple"}/>
+                        </StatisticsFrame>
+                    </div>
+                    <div className={"row"}>
+                        <StatisticsFrame header={"Playlist Statistics"}>
+                            <PlaylistStatisticsItem name={"My Playlist"}
+                                                    minutes={762}
+                                                    songs={254}
+                                                    top_artist={"Coldplay"}
+                                                    top_genre={"Pop"}
+                                                    top_artist_count={21}
+                                                    top_genre_count={86}/>
+                        </StatisticsFrame>
+                    </div>
                 </div>
             </div>
         </div>
