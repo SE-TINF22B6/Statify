6392242d8cc53722d5f55de6d59f6fad1efa66c8
Viktoriia <106541016+fehlend0@users.noreply.github.com>
2024-05-26 13:56:44 +0200

    Update statify_db.sql

--- Database/statify_db.sql
+++ Database/statify_db.sql
@@ -11,16 +11,19 @@ USE statify_db;
 
 CREATE TABLE `user` (
  guid CHAR(36) NOT NULL,
- access_token TEXT NOT NULL,
+ access_token TEXT NOT NULL UNIQUE,
+ refresh_token TEXT NOT NULL UNIQUE,
+ user_id TEXT UNIQUE,
  PRIMARY KEY (`guid`)
  );
 
-CREATE TABLE playlist_stats (
+CREATE TABLE playlists (
  guid CHAR(36) NOT NULL,
  user_guid CHAR(36),
+ playlist_id TEXT NOT NULL UNIQUE,
  `name` TEXT NOT NULL,
  tracks_number INT NOT NULL,
- duration_ms INT NOT NULL,
+ duration INT NOT NULL,
  top_genre TEXT NOT NULL,
  top_genre_tracks_number INT,
  top_artist TEXT NOT NULL,
@@ -29,75 +32,20 @@ CREATE TABLE playlist_stats (
  FOREIGN KEY (user_guid) REFERENCES `user`(`guid`)
  );
 
-CREATE TABLE artist (
- guid CHAR(36) NOT NULL,
- `name` TEXT NOT NULL,
- image_url TEXT,
- PRIMARY KEY (`guid`)
- );
-
-CREATE TABLE top_artists_stats (
+CREATE TABLE top_artist (
  guid CHAR(36) NOT NULL,
  user_guid CHAR(36),
- first_artist CHAR(36),
- second_artist CHAR(36),
- third_artist CHAR(36),
- fourth_artist CHAR(36),
- fifth_artist CHAR(36),
- start_date DATE NOT NULL,
- end_date DATE NOT NULL,
+ artist_id TEXT NOT NULL UNIQUE,
+ generate_date DATE NOT NULL,
  PRIMARY KEY (`guid`),
- FOREIGN KEY (user_guid) REFERENCES `user`(`guid`),
- FOREIGN KEY (first_artist) REFERENCES artist(`guid`),
- FOREIGN KEY (second_artist) REFERENCES artist(`guid`),
- FOREIGN KEY (third_artist) REFERENCES artist(`guid`),
- FOREIGN KEY (fourth_artist) REFERENCES artist(`guid`),
- FOREIGN KEY (fifth_artist) REFERENCES artist(`guid`)
+ FOREIGN KEY (user_guid) REFERENCES `user`(`guid`)
  );
 
-CREATE TABLE audio_feautures(
- guid CHAR(36) NOT NULL,
- duration_ms INT,
- `key` INT,
- tempo FLOAT,
- `mode` INT,
- danceability FLOAT,
- energy FLOAT,
- loudness FLOAT,
- speechiness FLOAT, 
- acousticness FLOAT, 
- instrumentalness FLOAT,
- liveness FLOAT,
- valence FLOAT,
- PRIMARY KEY (`guid`)
-);
-
-CREATE TABLE track (
- guid CHAR(36) NOT NULL,
- `name` TEXT NOT NULL,
- artist_name TEXT NOT NULL,
- image_url TEXT, 
- popularity INT,
- audio_features CHAR(36), 
- PRIMARY KEY (`guid`),
- FOREIGN KEY (audio_features) REFERENCES audio_feautures(`guid`)
- );
-
-CREATE TABLE top_tracks_stats(
+CREATE TABLE top_track (
  guid CHAR(36) NOT NULL,
  user_guid CHAR(36),
- first_track CHAR(36),
- second_track CHAR(36),
- third_track CHAR(36),
- fourth_track CHAR(36),
- fifth_track CHAR(36),
- start_date DATE NOT NULL,
- end_date DATE NOT NULL,
+ track_id TEXT NOT NULL UNIQUE,
+ generate_date DATE NOT NULL,
  PRIMARY KEY (`guid`),
- FOREIGN KEY (user_guid) REFERENCES `user`(`guid`),
- FOREIGN KEY (first_track) REFERENCES track(`guid`),
- FOREIGN KEY (second_track) REFERENCES track(`guid`),
- FOREIGN KEY (third_track) REFERENCES track(`guid`),
- FOREIGN KEY (fourth_track) REFERENCES track(`guid`),
- FOREIGN KEY (fifth_track) REFERENCES track(`guid`)
+ FOREIGN KEY (user_guid) REFERENCES `user`(`guid`)
 );
