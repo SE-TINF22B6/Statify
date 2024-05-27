359a40a9e1f7b4ae4e2eec56a96d3ca59d8f39b0
Ines Storck <storck.ines@gmail.com>
2024-05-27 11:11:27 +0200

    Added endpoints for statistics

--- backend_service/src/main/java/fm/statify/backend_service/controller/StatifyController.java
+++ backend_service/src/main/java/fm/statify/backend_service/controller/StatifyController.java
@@ -1,19 +1,20 @@
 package fm.statify.backend_service.controller;
 
+import fm.statify.backend_service.stats.PlaylistStatistics;
+import fm.statify.backend_service.stats.TopArtistStatistics;
+import fm.statify.backend_service.stats.TopTrackStatistics;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class StatifyController {
 
-    // Handler-Methode für die Startseite des Projekts
-    @GetMapping("/home")
-    public String homePage() {
-        return "home"; // Rückgabe des Namens der HTML-Seite (z.B. "home.html")
-    }
 
     // Handler-Methode für die Generierung von Statistiken über Songs
     @GetMapping("/songs")
@@ -29,6 +30,40 @@ public class StatifyController {
     public String generateArtistStatistics() {
         // Hier könnte Logik zur Generierung von Künstler-Statistiken stehen
         return "Artist statistics generated successfully!";
+    }
+
+    @GetMapping("statistics/artists")
+    @ResponseBody
+    public List<TopArtistStatistics> getTopArtistsStatistics(String userId){
+        //todo get top artist statistics for user from database
+
+        List<TopArtistStatistics> list = new ArrayList<>();
+        list.add(new TopArtistStatistics());
+        list.add(new TopArtistStatistics());
+
+        return list;
+    }
+    @GetMapping("statistics/tracks")
+    @ResponseBody
+    public List<TopTrackStatistics> getTopTracksStatistics(String userId){
+        //todo get top tracks statistics for user from database
+
+        List<TopTrackStatistics> list = new ArrayList<>();
+        list.add(new TopTrackStatistics());
+        list.add(new TopTrackStatistics());
+
+        return list;
+    }
+    @GetMapping("statistics/playlists")
+    @ResponseBody
+    public List<PlaylistStatistics> getPlaylistStatistics(String userId){
+        //todo get playlist statistics for user from database
+
+        List<PlaylistStatistics> list = new ArrayList<>();
+        list.add(new PlaylistStatistics());
+        list.add(new PlaylistStatistics());
+
+        return list;
     }
 }
 
