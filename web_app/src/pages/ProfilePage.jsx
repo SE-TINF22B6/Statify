671fa7be539507d5c114bf675bdda8d2d462cf5a
Ines Storck <storck.ines@gmail.com>
2024-06-02 15:45:53 +0200

    Use getProfile method in dataManager

--- web_app/src/pages/ProfilePage.jsx
+++ web_app/src/pages/ProfilePage.jsx
@@ -4,6 +4,7 @@ import profile from "../images/profile-icon.png"
 import Button from "../components/button";
 import {useEffect, useState} from "react";
 import {useNavigate, useSearchParams} from "react-router-dom";
+import {getProfile} from "../util/dataManager";
 
 export default function ProfilePage(){
 
@@ -12,12 +13,9 @@ export default function ProfilePage(){
     const [user, setUser] = useState({})
 
     useEffect(() => {
-        fetch("http://localhost:8081/profile?code=" + searchParams.get("code"))
-            .then((result) => {
-                return result.json()
-            })
-            .then((user) => {
-                setUser(user);
+        getProfile()
+            .then( result => {
+                setUser(result)
             })
     });
 
