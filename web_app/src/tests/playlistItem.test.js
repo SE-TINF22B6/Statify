6dc788bc6e37ca18e5cbe1c047b7590dafb2afc1
Ines Storck <storck.ines@gmail.com>
2024-06-08 16:51:30 +0200

    Fixed spelling

--- web_app/src/tests/playlistItem.test.js
+++ web_app/src/tests/playlistItem.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import PlaylistItem from '../components/PlaylistItem';
+import PlaylistItem from '../components/playlistItem';
 
 describe('PlaylistItem Component', () => {
     const title = "Test Title";
