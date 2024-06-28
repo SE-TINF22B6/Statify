30ce80882e33a5c3249b70c0c6ec5ef698187acc
Viktoriia <106541016+fehlend0@users.noreply.github.com>
2024-06-29 00:23:28 +0200

    added more tests for Parser

--- backend_service/src/test/java/fm/statify/backend_service/util/ParserTest.java
+++ backend_service/src/test/java/fm/statify/backend_service/util/ParserTest.java
@@ -1,7 +1,11 @@
 package fm.statify.backend_service.util;
 
+import fm.statify.backend_service.entities.Artist;
 import fm.statify.backend_service.entities.Playlist;
+import fm.statify.backend_service.entities.SimpleTrack;
 import fm.statify.backend_service.entities.UserProfile;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
 import java.util.List;
@@ -43,5 +47,71 @@ public class ParserTest {
         assertEquals("playlist1", playlist.getId());
         assertEquals("My Playlist", playlist.getName());
         assertEquals("https://example.com/playlist.jpg", playlist.getImageURL());
+    }
+
+    @Test
+    public void testParseTopTracks() {
+        String topTracksJsonString = "{\"items\":[{\"id\":\"track1\",\"name\":\"Track 1\",\"album\":{\"images\":[{\"url\":\"https://example.com/track1.jpg\"}]},\"artists\":[{\"name\":\"Artist 1\"}]}]}";
+
+        Parser parser = new Parser();
+
+        List<SimpleTrack> topTracks = parser.parseTopTracks(topTracksJsonString);
+
+        assertNotNull(topTracks);
+        assertEquals(1, topTracks.size());
+
+        SimpleTrack track = topTracks.get(0);
+        assertEquals("track1", track.getId());
+        assertEquals("Track 1", track.getName());
+        assertEquals("https://example.com/track1.jpg", track.getImageUrl());
+        assertEquals(1, track.getArtists().size());
+        assertEquals("Artist 1", track.getArtists().get(0));
+    }
+
+    @Test
+    public void testParseSimpleTrack() throws JSONException {
+        String trackJsonString = "{\"id\":\"track1\",\"name\":\"Track 1\",\"album\":{\"images\":[{\"url\":\"https://example.com/track1.jpg\"}]},\"artists\":[{\"name\":\"Artist 1\"}]}";
+
+        Parser parser = new Parser();
+
+        SimpleTrack simpleTrack = parser.parseSimpleTrack(new JSONObject(trackJsonString));
+
+        assertNotNull(simpleTrack);
+        assertEquals("track1", simpleTrack.getId());
+        assertEquals("Track 1", simpleTrack.getName());
+        assertEquals("https://example.com/track1.jpg", simpleTrack.getImageUrl());
+        assertEquals(1, simpleTrack.getArtists().size());
+        assertEquals("Artist 1", simpleTrack.getArtists().get(0));
+    }
+
+    @Test
+    public void testParseTopArtists() {
+        String topArtistsJsonString = "{\"items\":[{\"id\":\"artist1\",\"name\":\"Artist 1\",\"images\":[{\"url\":\"https://example.com/artist1.jpg\"}]}]}";
+
+        Parser parser = new Parser();
+
+        List<Artist> topArtists = parser.parseTopArtists(topArtistsJsonString);
+
+        assertNotNull(topArtists);
+        assertEquals(1, topArtists.size());
+
+        Artist artist = topArtists.get(0);
+        assertEquals("artist1", artist.getId());
+        assertEquals("Artist 1", artist.getName());
+        assertEquals("https://example.com/artist1.jpg", artist.getImageUrl());
+    }
+
+    @Test
+    public void testParseArtist() {
+        String artistJsonString = "{\"id\":\"artist1\",\"name\":\"Artist 1\",\"images\":[{\"url\":\"https://example.com/artist1.jpg\"}]}";
+
+        Parser parser = new Parser();
+
+        Artist artist = parser.parseArtist(artistJsonString);
+
+        assertNotNull(artist);
+        assertEquals("artist1", artist.getId());
+        assertEquals("Artist 1", artist.getName());
+        assertEquals("https://example.com/artist1.jpg", artist.getImageUrl());
     }
 }
